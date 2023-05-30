5cef707e53baaeecb6cd815a97d9f11083163016
정인섭 <jis1218@joonggonara.co.kr>
2023-05-30 23:01:49 +0900

    feature: response를 List로 바꿔주는 메서드 추가

--- lib/statistics/statistics_menu.dart
+++ lib/statistics/statistics_menu.dart
@@ -1,9 +1,27 @@
+import 'dart:convert';
+
 class StatisticsMenu {
   final String menuName;
-  final String menuUrl;
+  final int order;
 
   StatisticsMenu({
     required this.menuName,
-    required this.menuUrl,
+    required this.order,
   });
+}
+
+List<StatisticsMenu> convertResponseToList(String responseString) {
+  print(responseString.runtimeType);
+  print(jsonDecode(responseString).runtimeType);
+  print(jsonDecode(jsonDecode(responseString)).runtimeType);
+  print(responseString);
+  List list = jsonDecode(jsonDecode(responseString));
+  print(list);
+
+  List<StatisticsMenu> result = list
+      .map((element) => StatisticsMenu(
+      menuName: element['menu_name'], order: element['order']))
+      .toList();
+
+  return result;
 }
